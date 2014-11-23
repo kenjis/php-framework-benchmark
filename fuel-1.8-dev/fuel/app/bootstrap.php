16c1e51ece4c79635e5dc51091193c705604af9d
kenjis <kenji.uui@gmail.com>
2014-11-24 08:18:30 +0900

    FuelPHP 1.8-dev

---
+++ fuel-1.8-dev/fuel/app/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+// Bootstrap the framework DO NOT edit this
+require COREPATH.'bootstrap.php';
+
+\Autoloader::add_classes(array(
+	// Add classes you want to override here
+	// Example: 'View' => APPPATH.'classes/view.php',
+));
+
+// Register the autoloader
+\Autoloader::register();
+
+/**
+ * Your environment.  Can be set to any of the following:
+ *
+ * Fuel::DEVELOPMENT
+ * Fuel::TEST
+ * Fuel::STAGING
+ * Fuel::PRODUCTION
+ */
+\Fuel::$env = (isset($_SERVER['FUEL_ENV']) ? $_SERVER['FUEL_ENV'] : \Fuel::DEVELOPMENT);
+
+// Initialize the framework with the config file.
+\Fuel::init('config.php');
---
+++ fuel-1.8-dev/public/index.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.7
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2014 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+/**
+ * Set error reporting and display errors settings.  You will want to change these when in production.
+ */
+error_reporting(-1);
+ini_set('display_errors', 1);
+
+/**
+ * Website document root
+ */
+define('DOCROOT', __DIR__.DIRECTORY_SEPARATOR);
+
+/**
+ * Path to the application directory.
+ */
+define('APPPATH', realpath(__DIR__.'/../fuel/app/').DIRECTORY_SEPARATOR);
+
+/**
+ * Path to the default packages directory.
+ */
+define('PKGPATH', realpath(__DIR__.'/../fuel/packages/').DIRECTORY_SEPARATOR);
+
+/**
+ * The path to the framework core.
+ */
+define('COREPATH', realpath(__DIR__.'/../fuel/core/').DIRECTORY_SEPARATOR);
+
+// Get the start time and memory for use later
+defined('FUEL_START_TIME') or define('FUEL_START_TIME', microtime(true));
+defined('FUEL_START_MEM') or define('FUEL_START_MEM', memory_get_usage());
+
+// Load in the Fuel autoloader
+if ( ! file_exists(COREPATH.'classes'.DIRECTORY_SEPARATOR.'autoloader.php'))
+{
+	die('No composer autoloader found. Please run composer to install the FuelPHP framework dependencies first!');
+}
+
+require COREPATH.'classes'.DIRECTORY_SEPARATOR.'autoloader.php';
+class_alias('Fuel\\Core\\Autoloader', 'Autoloader');
+
+// Boot the app
+require APPPATH.'bootstrap.php';
+
+// Generate the request, execute it and send the output.
+try
+{
+	$response = \Request::forge()->execute()->response();
+}
+catch (\HttpNotFoundException $e)
+{
+	\Request::reset_request(true);
+
+	$route = array_key_exists('_404_', \Router::$routes) ? \Router::$routes['_404_']->translation : \Config::get('routes._404_');
+
+	if ($route instanceof Closure)
+	{
+		$response = $route();
+
+		if( ! $response instanceof \Response)
+		{
+			$response = \Response::forge($response);
+		}
+	}
+	elseif ($route)
+	{
+		$response = \Request::forge($route, false)->execute()->response();
+	}
+	else
+	{
+		throw $e;
+	}
+}
+
+// Render the output
+$response->body((string) $response);
+
+// This will add the execution time and memory usage to the output.
+// Comment this out if you don't use it.
+if (strpos($response->body(), '{exec_time}') !== false or strpos($response->body(), '{mem_usage}') !== false)
+{
+	$bm = \Profiler::app_total();
+	$response->body(
+		str_replace(
+			array('{exec_time}', '{mem_usage}'),
+			array(round($bm[0], 4), round($bm[1] / pow(1024, 2), 3)),
+			$response->body()
+		)
+	);
+}
+
+$response->send(true);
