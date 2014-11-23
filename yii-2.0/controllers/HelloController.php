63ef88df33063b162dec7e0e201b4a3bbdaa11a3
kenjis <kenji.uui@gmail.com>
2014-11-23 16:14:18 +0900

    Add controller hello and output peak memory

---
+++ yii-2.0/controllers/HelloController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class HelloController extends Controller
+{
+    public function actionIndex()
+    {
+        return 'Hello World!';
+    }
+}
--- yii-2.0/web/index.php
+++ yii-2.0/web/index.php
@@ -10,3 +10,5 @@ require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 $config = require(__DIR__ . '/../config/web.php');
 
 (new yii\web\Application($config))->run();
+
+echo "\n" . (memory_get_peak_usage(true)/1024/1024);
