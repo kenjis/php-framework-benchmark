396c53f3c5ff5db6e28c03700250a6ad78e99851
kenjis <kenji.uui@gmail.com>
2014-11-25 08:39:53 +0900

    Output benchmark command

--- benchmarks/_functions.sh
+++ benchmarks/_functions.sh
@@ -5,10 +5,11 @@ benchmark ()
     ab_log="output/$fw.ab.log"
     output="output/$fw.output"
 
-    echo $url
+    echo "ab -c 10 -t 10 $url"
     ab -c 10 -t 10 "$url" > "$ab_log"
     curl "$url" > "$output"
     rps=`grep "Requests per second:" "$ab_log" | cut -f 7 -d " "`
     m=`tail -1 "$output"`
     echo "$fw: $rps: $m" >> "$results"
+    echo
 }
