e2aa285dccfa92a59399311749b556db2ceb6a11
kenjis <kenji.uui@gmail.com>
2014-11-24 14:57:11 +0900

    Change order

--- benchmarks/hello_world.sh
+++ benchmarks/hello_world.sh
@@ -23,14 +23,14 @@ fw="fuel-1.8-dev"
 url="$base/$fw/public/index.php/hello/index"
 benchmark "$fw" "$url"
 
-fw="laravel-4.2"
-url="$base/$fw/public/index.php/hello/index"
-benchmark "$fw" "$url"
-
 fw="cake-3.0-dev"
 url="$base/$fw/index.php/hello/index"
 benchmark "$fw" "$url"
 
 fw="symfony-2.5"
 url="$base/$fw/web/app.php/hello/index"
+benchmark "$fw" "$url"
+
+fw="laravel-4.2"
+url="$base/$fw/public/index.php/hello/index"
 benchmark "$fw" "$url"
