3d3908e233a68f4a798ad8b6f151a456aa95f269
kenjis <kenji.uui@gmail.com>
2014-11-26 11:45:06 +0900

    Laravel 5.0-dev

---
+++ laravel-5.0-dev/app/Exceptions/Handler.php
@@ -0,0 +1,33 @@
+<?php namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler {
+
+	/**
+	 * Report or log an exception.
+	 *
+	 * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+	 *
+	 * @param  \Exception  $e
+	 * @return void
+	 */
+	public function report(Exception $e)
+	{
+		return parent::report($e);
+	}
+
+	/**
+	 * Render an exception into a response.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Exception  $e
+	 * @return \Illuminate\Http\Response
+	 */
+	public function render($request, Exception $e)
+	{
+		return parent::render($request, $e);
+	}
+
+}
---
+++ laravel-5.0-dev/app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,25 @@
+<?php namespace App\Http\Controllers;
+
+class WelcomeController extends Controller {
+
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('guest');
+	}
+
+	/**
+	 * Show the application welcome screen to the user.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return view('welcome');
+	}
+
+}
