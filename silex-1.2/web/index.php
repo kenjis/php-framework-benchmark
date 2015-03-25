34d70d5cfcc8c867d0de5e1e1dff00ad7b20bda6
kenjis <kenji.uui@gmail.com>
2015-03-25 10:53:31 +0900

    Add hello world and output peak memory

--- silex-1.2/web/index.php
+++ silex-1.2/web/index.php
@@ -4,6 +4,10 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-// ... definitions
+$app->get('/', function() {
+    return 'Hello World!';
+});
 
 $app->run();
+
+echo "\n" . (memory_get_peak_usage(true)/1024/1024);
