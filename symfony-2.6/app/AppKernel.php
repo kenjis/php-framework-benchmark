c7d63a4a237c259ddeea5254aa9a7722c50544b3
kenjis <kenji.uui@gmail.com>
2015-04-15 14:16:30 +0900

    Remove annotation for routes

--- symfony-2.6/app/AppKernel.php
+++ symfony-2.6/app/AppKernel.php
@@ -15,7 +15,7 @@ class AppKernel extends Kernel
 //            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
 //            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
 //            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+//            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
         );
 
