3a98ebc7cb872c2075fb13be91a1767271a0579d
kenjis <kenji.uui@gmail.com>
2015-04-15 10:00:46 +0900

    Revert to annotation for route

--- symfony-2.6/src/AppBundle/Controller/HelloController.php
+++ symfony-2.6/src/AppBundle/Controller/HelloController.php
@@ -2,11 +2,15 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class HelloController extends Controller
 {
+    /**
+     * @Route("/hello/index", name="hello_world")
+     */
     public function indexAction()
     {
         return new Response('Hello World!');
