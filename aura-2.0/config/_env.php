e2ab82d1ff8d4f4d4b0c86065992af66f3935e37
kenjis <kenji.uui@gmail.com>
2015-04-18 13:04:21 +0900

    Set config mode prod

--- aura-2.0/config/_env.php
+++ aura-2.0/config/_env.php
@@ -2,5 +2,5 @@
 // set the mode here only if it is not already set.
 // this allows for setting via web server, integration testing, etc.
 if (! isset($_ENV['AURA_CONFIG_MODE'])) {
-    $_ENV['AURA_CONFIG_MODE'] = getenv('AURA_CONFIG_MODE') ?: 'dev';
+    $_ENV['AURA_CONFIG_MODE'] = getenv('AURA_CONFIG_MODE') ?: 'prod';
 }
