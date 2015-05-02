cef7fc47c35297d2903402988c51021871803c5d
kenjis <kenji.uui@gmail.com>
2015-05-02 16:51:23 +0900

    Add script to show "Comparision before and after optimization" table

---
+++ bin/show_comparison_table.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__ . '/../libs/parse_results.php';
+require __DIR__ . '/../libs/build_table.php';
+require __DIR__ . '/../libs/recalc_relative.php';
+
+$list = [
+    'cake-3.0',
+    'symfony-2.6',
+    'zf-2.4',
+];
+
+system('git checkout master');
+$results_master = parse_results(__DIR__ . '/../output/results.hello_world.log');
+system('git checkout optimize');
+$results_optimize = parse_results(__DIR__ . '/../output/results.hello_world.log');
+//var_dump($results_master, $results_optimize);
+
+foreach ($list as $fw) {
+    $results = [];
+    $results[$fw] = $results_master[$fw];
+    $results[$fw . ' (*)'] = $results_optimize[$fw];
+    
+    $results = recalc_relative($results);
+    echo build_table($results) . "\n";
+}
