e1c0eb668fdd95a3ce414d09b853eb9007b9f3d9
kenjis <kenji.uui@gmail.com>
2015-05-02 16:28:14 +0900

    Refactor show_results_table.php

--- bin/show_results_table.php
+++ bin/show_results_table.php
@@ -1,20 +1,9 @@
 <?php
 
 require __DIR__ . '/../libs/parse_results.php';
+require __DIR__ . '/../libs/build_table.php';
 
 $results = parse_results(__DIR__ . '/../output/results.hello_world.log');
 //var_dump($results);
 
-echo '|framework          |requests per second|relative|peak memory|relative|' . "\n";
-echo '|-------------------|------------------:|-------:|----------:|-------:|' . "\n";
-
-foreach ($results as $fw => $result) {
-    printf(
-        "|%-19s|%19s|%8s|%11s|%8s|\n",
-        $fw,
-        number_format($result['rps'], 2),
-        number_format($result['rps_relative'], 1),
-        number_format($result['memory'], 2),
-        number_format($result['memory_relative'], 1)
-    );
-}
+echo build_table($results);
---
+++ libs/build_table.php
@@ -0,0 +1,22 @@
+<?php
+
+function build_table($results)
+{
+    $table = '';
+
+    $table .= '|framework          |requests per second|relative|peak memory|relative|' . "\n";
+    $table .= '|-------------------|------------------:|-------:|----------:|-------:|' . "\n";
+
+    foreach ($results as $fw => $result) {
+        $table .= sprintf(
+            "|%-19s|%19s|%8s|%11s|%8s|\n",
+            $fw,
+            number_format($result['rps'], 2),
+            number_format($result['rps_relative'], 1),
+            number_format($result['memory'], 2),
+            number_format($result['memory_relative'], 1)
+        );
+    }
+
+    return $table;
+}
