258d62c18f42f054979fdce654ad9be25962a48c
kenjis <kenji.uui@gmail.com>
2015-05-02 17:01:49 +0900

    Improve show_comparison_table.php

--- bin/show_comparison_table.php
+++ bin/show_comparison_table.php
@@ -16,11 +16,13 @@ system('git checkout optimize');
 $results_optimize = parse_results(__DIR__ . '/../output/results.hello_world.log');
 //var_dump($results_master, $results_optimize);
 
+$is_fisrt = true;
 foreach ($list as $fw) {
     $results = [];
     $results[$fw] = $results_master[$fw];
     $results[$fw . ' (*)'] = $results_optimize[$fw];
     
     $results = recalc_relative($results);
-    echo build_table($results) . "\n";
+    echo build_table($results, $is_fisrt);
+    $is_fisrt = false;
 }
