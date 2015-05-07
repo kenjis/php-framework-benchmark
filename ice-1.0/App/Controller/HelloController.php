3d4a8c066c545ee2203523c3e457c6a28e22675f
mruz <mruz@poczta.onet.pl>
2015-05-07 12:20:52 +0200

    Ice, use response instead of view, require Ice 1.0.12

--- ice-1.0/App/Controller/HelloController.php
+++ ice-1.0/App/Controller/HelloController.php
@@ -7,6 +7,6 @@ class HelloController extends IndexController
 
     public function indexAction()
     {
-        $this->view->setContent('Hello World!');
+        return $this->response->setContent('Hello World!');
     }
 }
