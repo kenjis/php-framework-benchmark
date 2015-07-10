884a377c9823958de18b5bd2092b9c70ffed9713
Omid Mottaghi <omidmr@gmail.com>
2015-07-10 17:00:03 +0430

    replace siege instead of ab, add warm up and cool down processes

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ if [ ! `which composer` ]; then
     exit 1;
 fi
 
-if [ ! `which ab` ]; then
-    echo "ab command not found."
+if [ ! `which siege` ]; then
+    echo "siege command not found."
     exit 1;
 fi
 
