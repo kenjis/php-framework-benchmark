5dc90f1e67494d39207c490ccb023e0de9e9f24e
julien <jr@corp.webedia.fr>
2015-10-14 19:32:58 +0200

    https://github.com/kenjis/php-framework-benchmark/pull/23#issuecomment-147876894

--- list.sh
+++ list.sh
@@ -9,7 +9,7 @@ slim-2.6
 codeigniter-3.0
 lumen-5.0
 yii-2.0
-silex-1.2
+#silex-1.2
 cygnite-1.3
 bear-1.0
 fuel-1.8-dev
@@ -25,5 +25,5 @@ laravel-5.1
 #fuel-2.0-dev
 zf-2.4
 typo3-flow-2.3
-nofussframework-1.2.3
+nofussframework-1.2
 "
