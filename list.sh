b9c874091f8786712208fba099b6a037bdfe76c5
kenjis <kenji.uui@gmail.com>
2015-10-15 06:48:44 +0900

    Update framework order

--- list.sh
+++ list.sh
@@ -7,6 +7,7 @@ phalcon-2.0
 ice-1.0
 slim-2.6
 codeigniter-3.0
+nofussframework-1.2
 slim-3.0
 bear-1.0
 radar-1.0-dev
@@ -16,6 +17,7 @@ silex-1.3
 cygnite-1.3
 lumen-5.0
 fuel-1.8-dev
+#fuel-2.0-dev
 cake-3.0
 cake-3.1
 aura-2.0
@@ -26,8 +28,6 @@ symfony-2.7
 #laravel-4.2
 #laravel-5.0
 laravel-5.1
-#fuel-2.0-dev
 zf-2.4
 typo3-flow-2.3
-nofussframework-1.2
 "
