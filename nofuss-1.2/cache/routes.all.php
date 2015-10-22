c9974bb0a5a1708e53f5fd576605ede647da36dc
kenjis <kenji.uui@gmail.com>
2015-10-22 10:06:31 +0900

    Fix route for nofuss-1.2

--- nofuss-1.2/cache/routes.all.php
+++ nofuss-1.2/cache/routes.all.php
@@ -7,9 +7,9 @@
       'home' => 
       array (
         'name' => 'home',
-        'regexp' => 'php-framework-benchmark/nofussframework-1.2/html/index.php/hello/index',
+        'regexp' => 'php-framework-benchmark/nofuss-1.2/html/index.php/hello/index',
         'controller' => 'home/index/index',
-        'inheritableRegexp' => 'nofussframework-1.2/html/index.php/hello/index',
+        'inheritableRegexp' => 'nofuss-1.2/html/index.php/hello/index',
       ),
     ),
   ),
