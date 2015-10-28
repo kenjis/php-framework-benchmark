7bab1f2c5948832ea605ae16ed4cdc81601d06a7
mruz <mruz@poczta.onet.pl>
2015-10-28 08:13:34 +0100

    Real number of included files by the framework

--- libs/output_data.php
+++ libs/output_data.php
@@ -4,5 +4,5 @@ printf(
     "\n%' 8d:%f:%d",
     memory_get_peak_usage(true),
     microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'],
-    count(get_included_files())
+    count(get_included_files()) - 1
 );
