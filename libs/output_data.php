0d46cc466fbd1cef4635f800d7adb9f1f5f88e0b
mruz <mruz@poczta.onet.pl>
2015-10-28 08:55:54 +0100

    Only the memory used by the framework

--- libs/output_data.php
+++ libs/output_data.php
@@ -2,7 +2,7 @@
 
 printf(
     "\n%' 8d:%f:%d",
-    memory_get_peak_usage(true),
+    memory_get_peak_usage(),
     microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'],
     count(get_included_files()) - 1
 );
