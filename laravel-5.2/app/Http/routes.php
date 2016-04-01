c63e91a9cff42dddc57af8e3ceb00e74ff62bbf7
kenjis <kenji.uui@gmail.com>
2016-04-01 19:12:56 +0900

    Add Hello controller

--- laravel-5.2/app/Http/routes.php
+++ laravel-5.2/app/Http/routes.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/hello/index', 'HelloController@index');
