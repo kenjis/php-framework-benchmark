5d17c5bb41397a7359a5bdc77fbce8d7068d6b93
kenjis <kenji.uui@gmail.com>
2016-04-10 20:32:57 +0900

    Remove the default hello route

--- fuel-1.8/fuel/app/config/routes.php
+++ fuel-1.8/fuel/app/config/routes.php
@@ -3,5 +3,5 @@ return array(
 	'_root_'  => 'welcome/index',  // The default route
 	'_404_'   => 'welcome/404',    // The main 404 route
 	
-	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+//	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );
