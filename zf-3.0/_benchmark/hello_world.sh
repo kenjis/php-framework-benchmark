05726f968d1f054469aa6285736fc8944c26aef2
seyfer <seyferseed@mail.ru>
2016-12-07 19:27:14 +0700

    fix

--- zf-3.0/_benchmark/hello_world.sh
+++ zf-3.0/_benchmark/hello_world.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-url="$base/$fw/public/application/hello/index"
+#doesn't work in v3
+#url="$base/$fw/public/application/hello/index"
+
+#will output Hello World!
+url="$base/$fw/public/"
+
