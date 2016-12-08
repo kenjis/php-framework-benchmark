1932f94002b19dbb75758a12d17c22c5d093812f
kenjis <kenji.uui@gmail.com>
2016-12-08 14:52:06 +0900

    Add rm -rf data/cache/*

--- zf-3.0/_benchmark/setup.sh
+++ zf-3.0/_benchmark/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 composer install --no-dev --optimize-autoloader
-composer development-enable # @TODO when I disable it, `/hello/index` shows 404. Why?
+composer development-disable
 chmod o+w data/cache/
+rm -rf data/cache/*
