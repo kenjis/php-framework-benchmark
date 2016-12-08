1273d9d0abbde905e96a14197320dd622855e2c6
kenjis <kenji.uui@gmail.com>
2016-12-08 10:20:59 +0900

    Workaround to 404

--- zf-3.0/_benchmark/setup.sh
+++ zf-3.0/_benchmark/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 composer install --no-dev --optimize-autoloader
-composer development-disable
+composer development-enable # @TODO when I disable it, `/hello/index` shows 404. Why?
 chmod o+w data/cache/
