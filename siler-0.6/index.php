400edc9eb2470a8a08f3124eee8f7e6475b67330
kenjis <kenji.uui@gmail.com>
2017-02-13 19:59:48 +0900

    Fix syntax error

--- siler-0.6/index.php
+++ siler-0.6/index.php
@@ -4,6 +4,6 @@ require 'vendor/autoload.php';
 
 Siler\Route\get('/hello/index', function () {
     echo 'Hello World!';
-})
+});
 
 require $_SERVER['DOCUMENT_ROOT'].'/php-framework-benchmark/libs/output_data.php';
