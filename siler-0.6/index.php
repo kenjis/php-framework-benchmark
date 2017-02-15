5b3c0707a47c846e9411653764494b6404b1f57e
kenjis <kenji.uui@gmail.com>
2017-02-15 21:01:17 +0900

    Fix siler-0.6 code
    Add pages/hello.php as something like controller

--- siler-0.6/index.php
+++ siler-0.6/index.php
@@ -1,9 +1,7 @@
 <?php
 
-require 'vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
-Siler\Route\get('/hello/index', function () {
-    echo 'Hello World!';
-});
+Siler\Route\get('/hello/index', './pages/hello.php');
 
 require $_SERVER['DOCUMENT_ROOT'].'/php-framework-benchmark/libs/output_data.php';
