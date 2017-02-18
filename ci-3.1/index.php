cebd09fc9fc9076282283c1418a65a340bb9da10
kenjis <kenji.uui@gmail.com>
2017-02-18 12:51:53 +0900

    Add output of memory and time

--- ci-3.1/index.php
+++ ci-3.1/index.php
@@ -313,3 +313,5 @@ switch (ENVIRONMENT)
  * And away we go...
  */
 require_once BASEPATH.'core/CodeIgniter.php';
+
+require $_SERVER['DOCUMENT_ROOT'].'/php-framework-benchmark/libs/output_data.php';
