de0e933791184c0d9f5254248c630fd64c276a06
N.T <nicktsai@talk2yam.com>
2017-07-24 21:41:41 +0800

    [yii-2.0] Switch to Asset-Packagist solution

--- yii-2.0/config/web.php
+++ yii-2.0/config/web.php
@@ -6,6 +6,10 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
