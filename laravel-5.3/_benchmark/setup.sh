cad880c6190b146b42bcd44c7e128839168cc2d3
Petr Bugyík <petr@bugyik.cz>
2017-09-29 17:39:22 +0200

    Add base Docker environment + remove sudo from some setup.sh scripts

--- laravel-5.3/_benchmark/setup.sh
+++ laravel-5.3/_benchmark/setup.sh
@@ -2,7 +2,7 @@
 
 composer install --no-dev --optimize-autoloader
 chmod o+w storage/*
-sudo chmod o+w storage/framework/*
+chmod o+w storage/framework/*
 php artisan optimize --force
 php artisan config:cache
 php artisan route:cache
--- staticphp-0.9/_benchmark/setup.sh
+++ staticphp-0.9/_benchmark/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo rm -rf Application/Cache/*
+rm -rf Application/Cache/*
 composer install --no-interaction --no-dev --optimize-autoloader
 
 chmod 777 -R Application/Cache/
