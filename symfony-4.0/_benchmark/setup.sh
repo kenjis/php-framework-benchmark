1c75910bb4f3d07a5c2afb980930ad8ccb0b2835
Tobias Nyholm <tobias.nyholm@gmail.com>
2018-06-16 11:02:49 +0200

    Warm up the cache before we run the tests

--- symfony-4.0/_benchmark/setup.sh
+++ symfony-4.0/_benchmark/setup.sh
@@ -4,5 +4,6 @@ export APP_ENV=prod
 export APP_DEBUG=0
 composer install --no-dev --optimize-autoloader
 php bin/console cache:clear --env=prod --no-debug
+php bin/console cache:warmup --env=prod --no-debug
 chmod o+w var/cache/ var/log/
 chmod -R o+w var/cache/*
